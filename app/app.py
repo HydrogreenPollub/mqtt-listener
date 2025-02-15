c8df20572eacc971848eeb835d0f15ee8c58eb31
Marek Kida <kidamarekk@gmail.com>
2025-02-15 15:54:35 +0100

    Add sample docker and python app

---
+++ app/app.py
@@ -0,0 +1,12 @@
+import time
+
+def main():
+    print("Starting Python application...")
+    while True:
+        print("Hello from Docker Compose!")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
+
+
