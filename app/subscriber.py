52ca857477a023b5261fd39935ec705917f16390
Marek Kida <ErgoAsh@users.noreply.github.com>
2025-05-20 15:22:15 +0000

    Remove topic leading character
    Add timestamp message
    Format code, remove unused parts.

--- app/subscriber.py
+++ app/subscriber.py
@@ -1,21 +1,18 @@
+import sys
+import time as tm
+import datetime
+import os
+import struct
+import numpy as np
 
 import paho.mqtt.client as mqtt
+from paho.mqtt.subscribeoptions import SubscribeOptions
 
 import psycopg2
 
-import struct
-import numpy as np
-from numpy.random import default_rng
 import flatbuffers
-# from flatc-generated files
 import TSData
 import FuelCellMode
-
-import sys
-import time as tm
-#import amqp
-import os
-
 
 print(os.getenv("BROKER_ADDRESS"))
 print(os.getenv("BROKER_PORT"))
@@ -27,17 +24,34 @@ print(os.getenv("DB_PASSWORD"))
 print(os.getenv("DB_HOST"))
 print(os.getenv("DB_PORT"))
 
-
-
 # Define the MQTT broker parameters
 broker_address = os.getenv("BROKER_ADDRESS")
-broker_port  = os.getenv("BROKER_PORT")
+broker_port = os.getenv("BROKER_PORT")
 username = os.getenv("BROKER_USERNAME")
 password = os.getenv("BROKER_PASSWORD")
-# conn_address = str(broker_address)+":"+str(broker_port)
 
+topic = "sensors"
 
-topic = "/sensors"
+def on_message(client, userdata, msg):
+    print('Received message')
+    buffer = bytearray(msg.payload)
+
+    ts_data = TSData.TSData.GetRootAs(buffer, 0)
+
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]  # [:-3] to get milliseconds instead of microseconds
+    print(f"=== Message received - {len(buffer)} bytes - {timestamp} (UTC) ===")
+    print(buffer.hex(sep=' '))
+
+    # TODO parse flatbuffers file dynamically by reading ts_data.fbs
+    cursor.execute(
+        "INSERT INTO measurements (time, vehicle_type, fc_voltage, fc_current, fc_temperature, sc_motor_voltage, sc_current, motor_current, motor_speed, motor_pwm, vehicle_speed, h2_pressure, h2_leak_level, fan_rpm, gps_latitude, gps_longitude, gps_altitude, gps_speed, lap_number) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        (tm.time(), '0', ts_data.FcVoltage(), ts_data.FcCurrent(), ts_data.FuelCellTemperature(), ts_data.ScVoltage(),
+         ts_data.FcScCurrent(), ts_data.MotorCurrent(), ts_data.MotorSpeed(), ts_data.MotorPwm(), ts_data.VehicleSpeed(),
+         ts_data.HydrogenPressure(), '2', ts_data.FanRpm(), ts_data.GpsLatitude(), ts_data.GpsLongitude(),
+         ts_data.GpsAltitude(), ts_data.GpsSpeed(), ts_data.LapNumber()))
+    conn.commit()
+
 
 try:
     conn = psycopg2.connect(
@@ -47,38 +61,20 @@ try:
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT")
     )
+
     cursor = conn.cursor()
-
-
-# Define the callback function to handle incoming messages
-    def on_message(client, userdata, msg):
-        print('Received message')
-        buf = bytearray(msg.payload)
-        tsdata = TSData.TSData.GetRootAs(buf,0)
-        print(tsdata.FcVoltage())
-    # Wstawianie danych
-        cursor.execute("INSERT INTO measurements (time, vehicle_type, fc_voltage, fc_current, fc_temperature, sc_motor_voltage, sc_current, motor_current, motor_speed, motor_pwm, vehicle_speed, h2_pressure, h2_leak_level, fan_rpm, gps_latitude, gps_longitude, gps_altitude, gps_speed, lap_number) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(tm.time(),'0',tsdata.FcVoltage(),tsdata.FcCurrent(),tsdata.FuelCellTemperature(),tsdata.ScVoltage(),tsdata.FcScCurrent(),tsdata.MotorCurrent(),tsdata.MotorSpeed(),tsdata.MotorPwm(),tsdata.VehicleSpeed(),tsdata.HydrogenPressure(),'2',tsdata.FanRpm(),tsdata.GpsLatitude(),tsdata.GpsLongitude(),tsdata.GpsAltitude(),tsdata.GpsSpeed(),tsdata.LapNumber()))
-        conn.commit()
-        print("Dane zostały wprowadzone!")
-
-
-#    with amqp.Connection(conn_address) as c:
-#        ch = c.channel()
-#        ch.basic_consume(queue='test', callback=on_message, no_ack=True)
-#        while True:
-#            c.drain_events()
 
     # Create an MQTT client
     new_client = mqtt.Client()
 
     # Set the username and password for authentication
     new_client.username_pw_set(username=username, password=password)
-    
+
     # Connect to the broker
     new_client.connect(host=broker_address, port=int(broker_port))
 
     # Subscribe to the topic
-    new_client.subscribe((topic, 0))
+    new_client.subscribe(topic, options=SubscribeOptions(qos=2))
 
     # Define the callback function to handle incoming messages
     new_client.on_message = on_message
@@ -89,6 +85,7 @@ try:
 
 except Exception as e:
     print("Błąd:", e)
+
 finally:
     cursor.close()
     conn.close()
