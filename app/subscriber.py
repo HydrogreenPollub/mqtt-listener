75bca3400647939d37e9035cb2811e7cc89d4d4f
Marek Kida <kidamarekk@gmail.com>
2025-06-04 15:02:57 +0200

    Update subscriber after local tests

--- app/subscriber.py
+++ app/subscriber.py
@@ -1,26 +1,24 @@
-import re
-import sys
-import time as tm
-import datetime
 import os
-import struct
-import numpy as np
-
+import re
+import time
+import datetime
 import paho.mqtt.client as mqtt
 from paho.mqtt.subscribeoptions import SubscribeOptions
 
+import flatbuffers
+from proto import TSData
+
+import capnp
+from tempfile import SpooledTemporaryFile
+
 import psycopg2
 
-import flatbuffers
-import TSData
-import FuelCellMode
-
-from tempfile import SpooledTemporaryFile
-import capnp
-#import ts_data_capnp
+# Uncomment for local tests
+from dotenv import load_dotenv
+load_dotenv()
 
 capnp.remove_import_hook()
-ts_data_capnp = capnp.load('./ts_data.capnp')
+ts_data_capnp = capnp.load('proto/ts_data.capnp')
 
 # Define the MQTT broker parameters
 broker_address = os.getenv("BROKER_ADDRESS")
@@ -28,103 +26,111 @@ broker_port = os.getenv("BROKER_PORT")
 username = os.getenv("BROKER_USERNAME")
 password = os.getenv("BROKER_PASSWORD")
 
-topic = "sensors"
+FRAME_SIZE = 160
 
 def to_snake_case(name):
     """Converts a CamelCase string to snake_case."""
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
+def get_curr_timestamp() -> str:
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]  # [:-3] to get milliseconds instead of microseconds
+
 def on_message(client, userdata, msg):
     buffer = bytearray(msg.payload)
 
-    now = datetime.datetime.now()
-    timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]  # [:-3] to get milliseconds instead of microseconds
-    print(f"=== Message received - {len(buffer)} bytes - {timestamp} (UTC) ===")
+    print(" ")
+    print(f"=== Message received - {len(buffer)} bytes - {get_curr_timestamp()} (UTC) ===")
     print(buffer.hex(sep=' '))
 
-    required_bytes = 128
-
-    if len(buffer) != required_bytes:
-        print(f"Required value not reached: frame save aborted (bytes: {len(buffer)}/{required_bytes})")
+    if len(buffer) != FRAME_SIZE:
+        print(f"Required value not reached: frame save aborted (bytes: {len(buffer)}/{FRAME_SIZE})")
         return
 
+    # Check message parsing via Capnp
     try:
-        ts_data_flatbuffers = TSData.TSData.GetRootAs(buffer)
-        print(f"[Data check - FlatBuffers] Latitude: {ts_data_flatbuffers.GpsLatitude()}, Longitude: {ts_data_flatbuffers.GpsLongitude()}")
-    except:
-        print("Flatbuffers conversion did not suceed") 
-
-    try:
-        f = SpooledTemporaryFile(256, 'wb+')
+        f = SpooledTemporaryFile(1024, 'wb+')
         f.write(buffer)
         f.seek(0)
         data = ts_data_capnp.TSData.read(f)
         ts_data = data.to_dict()
         print(f"data to dict: {ts_data}")
         print(f"[Data check - Capnp] Latitude: {ts_data["gpsLatitude"]}, Longitude: {ts_data["gpsLongitude"]}")
-    except:
-        print("Capnp conversion did not succeed") 
+    except Exception as err:
+        print("Capnp conversion did not succeed: ", err)
         return
 
-    snake_case_data = {to_snake_case(key): value for key, value in ts_data.items()}
+    # TODO Add custom columns to dictionary or remove unwanted here
+    ts_data["time"] = time.time_ns() # received time
 
-    column_names_str = ", ".join(snake_case_data)
-    placeholders_str = ", ".join(["%s"] * len(snake_case_data))
+    column_names_str = '"' +  '", "'.join(ts_data) + '"'
+    placeholders_str = ", ".join(["%s"] * len(ts_data))
+    values = tuple(ts_data.values())
 
     query = (
         f"INSERT INTO measurements ({column_names_str}) "
         f"VALUES ({placeholders_str}) "
         "ON CONFLICT DO NOTHING"
     )
-    values = tuple(value for key, value in ts_data.items())
+
+    print(f"Query: {query}")
+    print(f"Values: {values}")
 
     cursor.execute(query, values)
     conn.commit()
 
-try:
-    print("=== PROGRAM START ===")
-    print(f'BROKER_ADDRESS: {os.getenv("BROKER_ADDRESS")}')
-    print(f'BROKER_PORT: {os.getenv("BROKER_PORT")}')
-    print(f'DB_DATABASE: {os.getenv("DB_DATABASE")}')
-    print(f'DB_HOST: {os.getenv("DB_HOST")}')
-    print(f'DB_PORT: {os.getenv("DB_PORT")}')
+if __name__ == '__main__':
+    global cursor
+    try:
+        print(f"=== PROGRAM START - {get_curr_timestamp()} (UTC) ===")
+        print(f'BROKER_ADDRESS: {os.getenv("BROKER_ADDRESS")}')
+        print(f'BROKER_PORT: {os.getenv("BROKER_PORT")}')
+        print(f'MQTT_TOPIC: {os.getenv("MQTT_TOPIC")}')
+        print(f'DB_DATABASE: {os.getenv("DB_DATABASE")}')
+        print(f'DB_HOST: {os.getenv("DB_HOST")}')
+        print(f'DB_PORT: {os.getenv("DB_PORT")}')
 
-    conn = psycopg2.connect(
-        dbname=os.getenv("DB_DATABASE"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
-    )
-    print("Database connection was successful")
+        obj = time.gmtime(0)
+        epoch = time.asctime(obj)
+        print("Time epoch (time start):", epoch)
 
-    cursor = conn.cursor()
+        conn = psycopg2.connect(
+            dbname=os.getenv("DB_DATABASE"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+        print("Database connection was successful")
 
-    # Create an MQTT client
-    new_client = mqtt.Client()
+        cursor = conn.cursor()
 
-    # Set the username and password for authentication
-    new_client.username_pw_set(username=username, password=password)
+        # Create an MQTT client
+        new_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
-    # Connect to the broker
-    new_client.connect(host=broker_address, port=int(broker_port))
-    print("MQTT connection was successful")
+        # Set the username and password for authentication
+        new_client.username_pw_set(username=username, password=password)
 
-    # Subscribe to the topic
-    new_client.subscribe(topic, options=SubscribeOptions(qos=2))
+        # Connect to the broker
+        #new_client.tls_set()
+        new_client.connect(host=broker_address, port=int(broker_port))
+        print("MQTT connection was successful")
 
-    # Define the callback function to handle incoming messages
-    new_client.on_message = on_message
+        # Subscribe to the topic
+        new_client.subscribe(os.getenv("MQTT_TOPIC"), options=SubscribeOptions(qos=2))
 
-    # Start the loop to receive messages
-    new_client.loop_forever()
+        # Define the callback function to handle incoming messages
+        new_client.on_message = on_message
+
+        # Start the loop to receive messages
+        new_client.loop_forever()
 
 
-except Exception as e:
-    print("Error:", e)
+    except Exception as e:
+        print("Error:", e)
 
-finally:
-    cursor.close()
-    conn.close()
-    new_client.disconnect()
+    finally:
+        cursor.close()
+        conn.close()
+        new_client.disconnect()
